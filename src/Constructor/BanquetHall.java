0545f4474f01ddf92b8073be5b1ed01d8ed751d5
KamalDhillon <kkaur7887@gmail.com>
2022-02-25 16:04:30 -0800

    Correction

--- src/Constructor/BanquetHall.java
+++ src/Constructor/BanquetHall.java
@@ -1,38 +1,109 @@
 package Constructor;
 
 public class BanquetHall {
-	double totalBaseCost;
-	double totalCessTax;
-	double totalCost;
-	double totalBaseCostTax;
+	double baseCost;
+	double foodCost;
+	double beverageCost;
+	double tip;
+	double totalTaxApplied;
+	double totalCessApplied;
 
-	double calculateBaseCost(double foodCost, double beveragesCost, double tip) {
-		totalBaseCost = foodCost + beveragesCost + tip;
+	double foodTax;
+	double beverageTax;
+	double taxApplied;
+
+	// using Constructor to initialize foodTax,beverageTax,taxApplied
+	public BanquetHall(double foodTax, double beverageTax, double taxApplied) {
+		this.foodTax = foodTax;
+		this.beverageTax = beverageTax;
+		this.taxApplied = taxApplied;
+	}
+
+	public double getBaseCost() {
+		return baseCost;
+	}
+
+	public void setBaseCost(double baseCost) {
+		this.baseCost = baseCost;
+	}
+
+	public double getFoodCost() {
+		return foodCost;
+	}
+
+	public void setFoodCost(double foodCost) {
+		this.foodCost = foodCost;
+	}
+
+	public double getBeverageCost() {
+		return beverageCost;
+	}
+
+	public void setBeverageCost(double beverageCost) {
+		this.beverageCost = beverageCost;
+	}
+
+	// Calculating total Cost for food, beverages &baseCost including tax applied on
+	// them;
+	// Using +=operator for addition in String
+	double calculateBaseCost(double foodCost, double beveragesCost, double baseCost) {
+		this.foodCost += foodCost * foodTax;// cal. foodCost and storing value of food cost & tax applied on class level
+											// variable food Cost
+		this.beverageCost += tip + (beverageCost * beverageTax); // cal.beverageCost+ tax on beverages +tip and storing
+																	// value in class level beverage cost
+		double totalBaseCost = baseCost + this.foodCost + this.beverageCost;// cal totalBaseCost by using value we store
+																			// in instance level variable of
+																			// food&beverage.
 		return totalBaseCost;
 	}
 
-	// calculate tax as per alberta
-	double calculateTax(double totalBaseCost) {
-		this.totalBaseCostTax = totalBaseCost;
-		this.totalBaseCost = totalBaseCost * 0.5;
-		return this.totalBaseCostTax;
+	// calculate tax as per Alberta
+	private double calculateTax(double totalBaseCost) {
+		this.totalTaxApplied = totalBaseCost * 0.5; // calculating TaxApplied on BaseCost
+		return this.totalTaxApplied;
 	}
 
+	double getTotalTaxApplied(double totalBaseCost) {
+		return calculateTax(totalBaseCost);
+	}
+
+	// Checking conditions for noOfguset as CessApplied is based on noOfGuest
 	double calculateCess(int noOfGuest, double totalBaseCost) {
 		if (noOfGuest <= 40) {
-			totalCessTax = totalBaseCost * 0.04;
-		} else if (noOfGuest > 40 && noOfGuest <= 80) {
-			totalCessTax = totalBaseCost * 0.10;
+			return totalCessApplied = totalBaseCost * 0.04;
+		} else if (noOfGuest <= 80) {
+			return totalCessApplied = totalBaseCost * 0.08;
+		} else if (noOfGuest <= 150) {
+			return totalCessApplied = totalBaseCost * 0.10;
 		} else {
-			totalCessTax = totalBaseCost * 0.125;
-
+			return totalCessApplied = totalBaseCost * 0.125;
 		}
-		return totalCessTax;
 	}
 
-	double calculateTheTotalCost(double totalBaseCost, double totalBaseTax, double totalCessTax) {
-		totalCost = totalBaseCost + totalBaseTax + totalCessTax;
-		return totalCost;
+	double getCess(int noOfGuest, double totalBaseCost) {
+		return calculateCess(noOfGuest, totalBaseCost);
+	}
+
+	// Using return value of calcualteBAseCost, calculateTax & calCess to
+	// calculateTotalCost for BanquetHall
+	double calculateTheTotalCost(double totalBaseCost, double totalTaxApplied, double Cess, int noOfGuest) {
+		totalTaxApplied = calculateTax(totalBaseCost);
+		Cess = calculateCess(noOfGuest, totalBaseCost);
+		return totalBaseCost + totalTaxApplied + Cess;
+	}
+
+	void printInvoice(int guestCount, double totalBaseCost) {
+		System.out.println("     **** INVOICE DETAILS ****        ");
+		System.out.println("Base Booking Cost of the Hall :  $" + baseCost);
+		System.out.println("   Cost of Food(includes tax) :  $" + foodCost);
+		System.out.println(" 	      Beverage Cost ");
+		System.out.println("      (includes tax and tips) :  $" + beverageCost);
+		System.out.println("-----------------------------   ------------");
+		System.out.println("              Total Base Cost :  $" + totalBaseCost);
+		System.out.println("             Tax on Base Cost :  $" + calculateTax(totalBaseCost));
+		System.out.println(
+				"     Service Cess (" + guestCount + " people) :  $" + calculateCess(guestCount, totalBaseCost));
+		System.out.println("----------------------------- : ------------");
 	}
 
 }
--- src/Constructor/MainClass.java
+++ src/Constructor/MainClass.java
@@ -1,33 +1,60 @@
 package Constructor;
 
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    Scanner sc=new Scanner (System.in);
+		Scanner sc = new Scanner(System.in);
 
-      BanquetHall banquet=new BanquetHall();
-	 System.out.println("What's the cost for food"); 
-	 double foodCost=sc.nextDouble();
-	 
-	 System.out.println("What's the cost for beverages");
-	 double beveragesCost=sc.nextDouble();
-	 
-	 System.out.println("How much tip do you want to add");
-	 double tip=sc.nextDouble();
-	 
-	 double TotalBaseCost=banquet.calculateBaseCost(foodCost, beveragesCost, tip);
-	 double tax=banquet.calculateTax(TotalBaseCost);
-	 
-	 System.out.println("Enter no. of guest");
-	 int guest=sc.nextInt();
-	 
-	 double calculateCessTax=banquet.calculateCess(guest, TotalBaseCost);
-	 double theTotalCost=banquet.calculateTheTotalCost(TotalBaseCost, TotalBaseCost, calculateCessTax);
-	 System.out.println("Total cost for banquet Hall is:" +theTotalCost);
-			 
+		BanquetHall banquet = new BanquetHall(0.05, 0.05, 0.10);
+		System.out.println("Welcome to our Banquet Hall");
+		System.out.println("Please note our basic requirements to book hall");
+		System.out.println("1- BaseCost needs to be atleast 100$");
+		System.out.println("2-Food and beverages needs to be included in package , but tip is optional");
+		System.out.println("3_ tax on food and beverages will applied in addition to total Tax");
+		System.out.println("4-Guest Capacity=10 to 400 people");
+		System.out.println("Based on your requirements please enter following;");
+		System.out.println("Please enter Base Cost");
+		banquet.setBaseCost(sc.nextDouble());
+
+		if (banquet.getBaseCost() >= 100) {//checking condition for BaseCost
+			System.out.println("Please Enter cost for food");
+			banquet.setFoodCost(sc.nextDouble());
+
+			if (banquet.getFoodCost() > 0.0) {//condition for food cost
+				System.out.println("Please enter cost for beverages");
+				banquet.setBeverageCost(sc.nextDouble());
+
+				if (banquet.getBeverageCost() > 0.0) {//condition for tip
+					System.out.println("Please Enter tip amount");
+					banquet.tip = sc.nextDouble();
+					System.out.println(" Please Enter total number of guest");
+					int noOfGuest = sc.nextInt();
+
+					if (noOfGuest >= 10 && noOfGuest <= 400) {//condition for guest
+
+						double totalBaseCost = banquet.calculateBaseCost(banquet.getBaseCost(), banquet.getFoodCost(),
+								banquet.getBeverageCost());
+						double theTotalCost = banquet.calculateTheTotalCost(totalBaseCost,
+								banquet.getTotalTaxApplied(totalBaseCost), banquet.getCess(noOfGuest, totalBaseCost),
+								noOfGuest);
+						banquet.printInvoice(noOfGuest, totalBaseCost); //calling  method to print the detailed Invoice
+						System.out.println("               Invoice Amount :  $" + theTotalCost);
+						
+					} else {
+						System.out.println("Invalid entry for guest");
+					}
+				} else {
+					System.out.println("Invalid entry for beverages");
+				}
+			} else {
+				System.out.println("Invalid entry for food");
+			}
+		} else {
+			System.out.println("Invalid entry for Base Cost");
+		}
 	}
 
 }
